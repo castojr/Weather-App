4966cea2c3bcbd906d72d8dbd1c84cf9eb8a0061
Joey <castojr@yahoo.com>
2016-06-11 21:10:08 -0700

    added temp toggle & icon logic

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
-
   var lat;
   var lon;
+//Gets location coordinates 
+  if (navigator.geolocation) {   navigator.geolocation.getCurrentPosition(function(position) {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+//Concat longitude & latitude in api url
+      var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=916cd8dca1482357a8801dbdf0ca49c8";
 
-  
- if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    var url = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=916cd8dca1482357a8801dbdf0ca49c8";
- 
-  $.getJSON(url, function(data) {
-    
-    var tempF = Math.floor(data.main.temp * 9/5 - 459.67); 
-    var tempC = Math.floor(data.main.temp - 273.15);
-    var humd = data.main.humidity;
-    var city = data.name;
-    var windSpeed = data.wind.speed;
-    var weatherMain = data.weather.main;
-    var weatherDesc = data.weather.description;
-    var weatherIcon = data.weather.icon;
-    
-    $("#tempF").html(tempF + "\xB0" + "F");
-    $("#tempC").html(tempC + "\xB0" + "C");
-    $("#humd").html(humd + "%");
-    $("#city").html(city);
-    $("#windSpeed").html(windSpeed + "mph");
-    
-    $("#weatherMain").html(weatherMain);
-    $("#weatherDesc").html(weatherDesc);
-    $("#weatherIcon").html(weatherIcon);
-    
-  });
-  });
-} 
+      $.getJSON(url, function(data) {
+        var tempF = Math.floor(data.main.temp * 9 / 5 - 459.67);
+        var tempC = Math.floor(data.main.temp - 273.15);
+        var humd = data.main.humidity;
+        var city = data.name;
+        var windSpeed = data.wind.speed;
+        var weatherIcon = data.weather[0].icon;
+        var iconURL = "http://api.openweathermap.org/img/w/"+ weatherIcon +".png";
+
+        $("#tempF").html(tempF + "\xB0" + "F");
+        $("#humd").html(humd + "%");
+        $("#city").html(city);
+        $("#windSpeed").html("Wind Speed: " + Math.floor(windSpeed / 0.44704) + " mph");   
+        $("#weatherIcon").attr("src", iconURL);    
+        //switches temperature to Fahrenheit        
+        $("#option1").on("click", function() {
+          $("#tempF").html(tempF + "\xB0" + "F")
+        });
+        //switches temperature to Celsius   
+        $("#option2").on("click", function() {
+          $("#tempF").html(tempC + "\xB0" + "C")
+        });
+
+      });
+
+    });
+  }
 });
-
